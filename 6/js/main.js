b63d9627309e3306440a87c92dc303e98b55cb31
Keks <keks@htmlacademy.ru>
2022-07-04 15:30:20 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,5 @@
+import {objectList, getObjectList} from "./utils.js";
+// Задание 4 + модуляция
+// для проверки работоспособности использовалась console.log(objectList);
+getObjectList();
+console.log(objectList);
---
+++ 6/js/task 2/task2.js
@@ -0,0 +1,20 @@
+let random = function (a, b) {
+    if (a < 0) {
+      a = 0;
+    }
+  
+    return Math.random() * (b - a) + a;
+  
+  };
+  
+  let commentLength = function (string, maxLength) {
+  
+    if (string.length > maxLength) {
+      string = '';
+    } else {
+    }
+  
+  }
+  
+  random(0, 15);
+  commentLength('Второе домашнее задание успешно выполнено', 140);
