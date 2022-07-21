86e1539d413ebca266326c4272db8dbded2e2480
Даниил <den_kozhen1971@mail.ru>
2022-07-21 17:13:14 +0300

    Перламутровые пуговицы

--- js/api.js
+++ js/api.js
@@ -1,19 +1,21 @@
-import {generetePublicationsArray} from './publications.js';
 import { buttonDisabled} from './util.js';
-import {addSuccesfulMessage, addErrorMessage} from './formMessage.js';
+import {addSuccesfulMessage, addErrorMessage} from './form-message.js';
 import {closeModalWindow, formSubmitButton} from './form.js';
+import { generetePublicationsArray } from './publications.js';
 
 const DATA_SERVER = 'https://26.javascript.pages.academy/kekstagram/data';
 const SERVER = 'https://26.javascript.pages.academy/kekstagram';
 
 let publicationArray;
-fetch(DATA_SERVER)
-  .then((response) => response.json())
-  .then((publications) => {
-    publicationArray = publications;
-    generetePublicationsArray(publications);
-  });
-let isError = false;
+const generetePublications = () => {
+  fetch(DATA_SERVER)
+    .then((response) => response.json())
+    .then((publications) => {
+      publicationArray = publications;
+      generetePublicationsArray(publicationArray);
+    });
+};
+
 const sendForm = (formData) => {
   fetch(SERVER,
     {
@@ -23,14 +25,11 @@ const sendForm = (formData) => {
   )
     .catch(() => {
       addErrorMessage();
-      isError = true;
     }).then(() => {
-      if(!isError) {
-        buttonDisabled(formSubmitButton, 'Публикуется...');
-        closeModalWindow();
-        addSuccesfulMessage();
-      }
+      buttonDisabled(formSubmitButton, 'Публикуется...');
+      closeModalWindow();
+      addSuccesfulMessage();
     });
 };
 
-export {publicationArray, sendForm};
+export {publicationArray, sendForm, generetePublications};
