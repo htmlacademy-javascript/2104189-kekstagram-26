1f79a7db2d11850fd8481a3511b9911868679d26
Даниил <den_kozhen1971@mail.ru>
2022-07-25 16:23:38 +0300

    Фото на память

--- js/validation-functions.js
+++ js/validation-functions.js
@@ -1,37 +1,58 @@
 import {getIsStringSizeAvailable} from './util.js';
 
-// Checking for max 5 hashTags
-const hashTagCountValidate = (value) => {
+const HASHTAG_MAX_ITEMS = 5;
+const HASHTAG_MAX_LENGTH = 20;
+const COMMENT_MAX_LENGTH = 140;
+
+const validateHashTagCount = (value) => {
   const hashTags = value.split(' ');
-  if(hashTags.length > 5) {
+  if (hashTags.length > HASHTAG_MAX_ITEMS) {
     return false;
   }
   return true;
 };
 
-// Checking for regular expression
-const hashTagTextValidate = (value) => {
-  const hashTagTerms = /^#[A-Za-zA-Яа-яЁё0-9]{1,20}$/;
+const validateHashTagText = (value) => {
+  const hashTagTerms = /^#[A-Za-zA-Яа-яЁё0-9]{1,}$/;
   const hashTags = value.split(' ');
 
   if (hashTags[0] !== '') {
     let notValidCounter = 0;
     hashTags.forEach((hashTag) => {
-      if(!hashTagTerms.test(hashTag)) {
+      if (!hashTagTerms.test(hashTag)) {
         notValidCounter++;
       }
     });
-    if(notValidCounter > 0) {
+    if (notValidCounter > 0) {
       return false;
     }
   }
   return true;
 };
 
-// Checking for hashTags repetiton
-const hashTagRepeatValidate = (value) => {
+const validateHashTagSize = (value) => {
   const hashTags = value.split(' ');
-  const hahTagsSet = new Set (hashTags);
+
+  if (hashTags[0] !== '') {
+    let notValidCounter = 0;
+    hashTags.forEach((hashTag) => {
+      if (hashTag.length > HASHTAG_MAX_LENGTH) {
+        notValidCounter++;
+      }
+    });
+    if (notValidCounter > 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const validateHashTagRepeat = (value) => {
+  const hashTags = value.split(' ');
+  hashTags.forEach((hashTag, i) => {
+    hashTags[i] = hashTag.toLowerCase();
+  });
+  const hahTagsSet = new Set(hashTags);
   if (hahTagsSet.size !== hashTags.length) {
     return false;
   }
@@ -39,7 +60,6 @@ const hashTagRepeatValidate = (value) => {
   return true;
 };
 
-// Function for validation comments
-const commentValidate = (value) => getIsStringSizeAvailable(value, 140);
+const validateComment = (value) => getIsStringSizeAvailable(value, COMMENT_MAX_LENGTH);
 
-export {hashTagCountValidate, hashTagTextValidate, hashTagRepeatValidate, commentValidate};
+export {validateHashTagCount, validateHashTagText, validateHashTagRepeat, validateComment, validateHashTagSize};
